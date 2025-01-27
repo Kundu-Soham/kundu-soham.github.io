8fe1e0c3aef0cc1aa6da9349734bddefa16fd337
Soham Kundu <sohamputukundu@gmail.com>
2025-01-27 16:45:49 -0500

    Changed formatting of Projects.html page. Added 3 sections for each type of projects and linked home page to specific sections of projects page.

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // Go to projects Functions
 function goToMechProjects() {
-    window.location.href = "projects.html"
+    window.location.href = "projects.html#mech-projects"
 }
 
 function goToProgrammingProjects() {
-    window.location.href = "projects.html"
+    window.location.href = "projects.html#programming-projects"
 }
 
 function goToManagementProjects() {
-    window.location.href = "projects.html"
+    window.location.href = "projects.html#management-projects"
 }
