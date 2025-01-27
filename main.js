9e4e4cfa0062f89ba3637e0fce1b378713c4da5d
Soham Kundu <sohamputukundu@gmail.com>
2025-01-26 23:03:59 -0500

    Changes to formatting of both pages, added javascript file and pictures of CAD projects

---
+++ main.js
@@ -0,0 +1,12 @@
+// Go to projects Functions
+function goToMechProjects() {
+    window.location.href = "projects.html"
+}
+
+function goToProgrammingProjects() {
+    window.location.href = "projects.html"
+}
+
+function goToManagementProjects() {
+    window.location.href = "projects.html"
+}
